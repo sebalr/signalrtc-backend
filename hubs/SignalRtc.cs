eb51ba50b6b95d70d86e892f26281c50218aa251
sebalr <larrieu.sebastian@gmail.com>
2019-08-08 20:45:50 -0300

    SignalR Hub first signaling methods

--- hubs/SignalRtc.cs
+++ hubs/SignalRtc.cs
@@ -6,15 +6,15 @@ namespace signalRtc.hubs
     public class SignalRtcHub : Hub
     {
 
-        public async Task NewPeer(string signal, string group)
+        public async Task NewPeer(string group)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, group);
-            await Clients.OthersInGroup(group).SendAsync(signal, Context.ConnectionId);
+            await Clients.OthersInGroup(group).SendAsync("NewPeer", Context.ConnectionId);
         }
 
         public async Task SendSignal(string signal, string user)
         {
-            await Clients.Client(user).SendAsync(signal);
+            await Clients.Client(user).SendAsync("SendSignal", Context.ConnectionId, signal);
         }
     }
 }
