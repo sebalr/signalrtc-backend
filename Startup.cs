4b452c62e64d4e9de723a321aa9b58e5dc0fe4ee
sebalr <larrieu.sebastian@gmail.com>
2019-08-08 20:46:43 -0300

    Cors and ssl configuration

--- Startup.cs
+++ Startup.cs
@@ -16,8 +16,19 @@ namespace signalRtc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                    builder => builder.WithOrigins("http://localhost:4200", "https://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
             services.AddSignalR();
         }
+
+        readonly string MyAllowSpecificOrigins = "AllowOrigins";
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -26,6 +37,8 @@ namespace signalRtc
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseSignalR(x => x.MapHub<SignalRtcHub>("/signalrtc"));
 
