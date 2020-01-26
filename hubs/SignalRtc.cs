c26e21fd91be790a6e3cf4d93a019186885525d3
sebalr <larrieu.sebastian@gmail.com>
2020-01-26 10:28:55 -0300

    Rename events

--- hubs/SignalRtc.cs
+++ hubs/SignalRtc.cs
@@ -11,13 +11,13 @@ namespace signalRtc.hubs
         public async Task NewUser(string username)
         {
             var userInfo = new UserInfo() { userName = username, connectionId = Context.ConnectionId };
-            await Clients.Others.SendAsync("NewUser", JsonSerializer.Serialize(userInfo));
+            await Clients.Others.SendAsync("NewUserArrived", JsonSerializer.Serialize(userInfo));
         }
 
         public async Task HelloUser(string userName, string user)
         {
             var userInfo = new UserInfo() { userName = userName, connectionId = Context.ConnectionId };
-            await Clients.Client(user).SendAsync("HelloUser", JsonSerializer.Serialize(userInfo));
+            await Clients.Client(user).SendAsync("UserSaidHello", JsonSerializer.Serialize(userInfo));
         }
 
         public async Task SendSignal(string signal, string user)
